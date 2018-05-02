bcde3e0a540ad710e0f015364265f9febd009c42
yuyi1266173 <yuyi1266173@163.com>
2018-05-02 11:13:42 +0800

    add config file for project

---
+++ conf/config_default.py
@@ -0,0 +1,17 @@
+"""
+Default configurations.
+"""
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': "127.0.0.1",
+        "port": 3306,
+        "user": "root",
+        "password": "password",
+        "db": 'myblog'
+    },
+    "session": {
+        "secret": 'MyBlog'
+    }
+}
