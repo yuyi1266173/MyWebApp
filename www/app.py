19689965dbac22f7f4e6f747a4ce35ca83cd001a
yuyi1266173 <yuyi1266173@163.com>
2018-05-02 12:00:08 +0800

    add datatimefitle and auth_factory in app.py

--- www/app.py
+++ www/app.py
@@ -1,11 +1,18 @@
-import logging; logging.basicConfig(level=logging.INFO)
+import logging
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s] %(name)s:%(levelname)s: %(message)s")
 
 import asyncio, os, json, time
 from datetime import datetime
 
 from aiohttp import web
+# FileSystemLoader是文件系统加载器，用来加载模板路径
+from jinja2 import Environment, FileSystemLoader
+
 
 from ..conf.config import configs
+import orm
+from coroweb import add_routes, add_static
+from handlers import cookie2user, COOKIE_NAME
 
 
 # 这个函数的作用就是当有http请求的时候，通过logging.info输出请求的信息，其中包括请求的方法和路径
@@ -15,6 +22,35 @@ async def logger_factory(app, handler):
         # handler为处理函数，request为参数
         return await handler(request)
     return logger
+
+async def auth_factory(app, handler):
+    async def auth(request):
+        logging.info('check user:%s %s' % (request.method, request.path))
+        request.__user__ = None
+        # cookies是用分号分割的一组名值对，在python中被看成dict
+        cookie_str = request.cookies.get(COOKIE_NAME)
+        if cookie_str:
+            user = await cookie2user(cookie_str)
+            print(user)
+            if user:
+                logging.info('set current user:%s' % user.email)
+                request.__user__ = user
+        if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+            return web.HTTPFound('/signin')
+        return await handler(request)
+    return auth
+
+async def data_factory(app, handler):
+    async def parse_data(request):
+        if request.method == 'POST':
+            if request.content_type.startswith('application/json'):
+                request.__data__ = await request.json()
+                logging.info('request json:%s' % str(request.__data__))
+            elif request.content_type.startswith('application/x-www-form-urlencoded'):
+                request.__data__ = await request.post()
+                logging.info('request form : %s' % str(request.__data__))
+        return await handler(request)
+    return parse_data
 
 # 请求对象request的处理工序流水线先后依次是：
 #     	logger_factory->response_factory->RequestHandler().__call__->get或post->handler
@@ -69,17 +105,71 @@ async def response_factory(app, handler):
     return response
 
 
-def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>')
+def init__jinja2(app, **kw):
+    logging.info('init jinja2...')
+    options = dict(
+        # 自动转义xml/html的特殊字符
+        autoescape=kw.get('autoescape', True),
+        # 代码块的开始结束标志
+        block_start_string=kw.get('block_start_string', '{%'),
+        block_end_string=kw.get('block_end_string', '%}'),
+        # 变量的开始结束标志
+        variable_start_string=kw.get('variable_start_string', '{{'),
+        variable_end_string=kw.get('variable_end_string', '}}'),
+        # 当模板文件被修改后，下次请求加载该模板文件的时候会自动重新加载修改后的模板文件
+        auto_reload=kw.get('auto_reload', True)
+    )
+    # 获取模板文件的位置
+    path = kw.get('path', None)
+    if path is None:
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+    logging.info('set jinja2 template path : %s' % path)
+    # Environment是jinjia2中的一个核心类，它的实例用来保存配置、全局对象以及模板文件的路径
+    env = Environment(loader=FileSystemLoader(path), **options)
+    # filters: 一个字典描述的filters过滤器集合, 如果非模板被加载的时候, 可以安全的添加或较早的移除.
+    filters = kw.get('filters', None)
+    if filters is not None:
+        for name, f in filters.items():
+            env.filters[name] = f
+    # 所有的一切是为了给app添加__templating__字段
+    # 前面将jinja2的环境配置都赋值给env了，这里再把env存入app的dict中，这样app就知道要到哪儿去找模板，怎么解析模板。
+    app['__templating__'] = env
 
-@asyncio.coroutine
-def init(loop):
-    app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', index)
-    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
-    logging.info('server started at http://127.0.0.1:9000...')
+
+def datetime_filter(t):
+    delta = int(time.time() - t)
+    if delta < 60:
+        return u'1分钟前'
+    if delta < 3600:
+        return u'%s分钟前' % (delta//60)
+    if delta < 86400:
+        return u'%s小时前' % (delta//3600)
+    if delta < 604800:
+        return u'%s天前' % (delta//86400)
+    dt = datetime.fromtimestamp(t)
+    return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
+
+
+async def init(loop):
+    await orm.create_pool(loop=loop, **configs.db)
+    # middlewares(中间件)设置3个中间处理函数(都是装饰器)
+    # middlewares中的每个factory接受两个参数，app 和 handler(即middlewares中的下一个handler)
+    # 譬如这里logger_factory的handler参数其实就是auth_factory
+    # middlewares的最后一个元素的handler会通过routes查找到相应的，就是routes注册的对应handler处理函数
+    # 这是装饰模式的体现，logger_factory, auth_factory, response_factory都是URL处理函数前（如handler.index）的装饰功能
+    app = web.Application(loop=loop, middlewares=[logger_factory, auth_factory, response_factory])
+    init__jinja2(app, filters=dict(datetime=datetime_filter))
+    # 添加URL处理函数
+    add_routes(app, 'handlers')
+    # 添加CSS等静态文件路径
+    add_static(app)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 10000)
+
+    logging.info('Server started at http://127.0.0.1:10000')
     return srv
 
+# 获取eventloop
 loop = asyncio.get_event_loop()
+# 然后加入运行事件
 loop.run_until_complete(init(loop))
 loop.run_forever()
